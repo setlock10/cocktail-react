314b922102582a8a77632746756482f8ce8eb0e7
John J Setlock II <johnsetlock@gmail.com>
2022-08-20 07:46:59 -0500

    update

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+
+
+    return(
+        <header>
+         <div id="logo">
+            <h1 class="font-effect-neon">Cocktail</h1>
+            <h4 class="face-off-font">FACE/OFF</h4>
+        </div>
+        </header>
+    );
+}
+
+export default Header;
